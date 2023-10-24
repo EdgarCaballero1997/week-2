a1b1d2c1fb60d715f6e8b9c67169c2f580b7755d
Edgar Caballero <ecaballeromora@outlook.com>
2023-10-24 03:59:03 +0200

    Resubida retos-repaso1

---
+++ retos-repaso1/buclesFor.ts
@@ -0,0 +1,51 @@
+// 2. Realizar una función que imprima los números impares existentes hasta el número
+// indicado como parámetro de entrada.
+
+// La cabecera de la función tendrá el siguiente aspecto: function evenNumbers(num)
+function everNumber(number: number){
+    let listaNumeros: number[] = [];
+    for(let i = 0; i < number; i += 3){
+        listaNumeros.push(i);
+    }
+    return listaNumeros;
+}
+console.log(everNumber(100));
+// Realizar una función que como parámetro de entrada reciba un array de strings y como
+// salida devuelva el array revertido. No se puede utilizar el método revert de la clase array
+// La cabecera de la función tendrá el siguiente aspecto: function myRevert(myArr)
+function myRevert(array: string[]){
+    let nameList: string[] = [];
+    for(let i = 4; i > -1; i--){
+        nameList.push(array[i]);
+    }
+    return nameList;
+}
+let listName: string[] = ["Pepe", "Juan", "Marta", "Lorena", "Maria"];
+console.log(myRevert(listName));
+// Realizar una función que reciba como parámetro un array de strings que contenga
+// nombres de colores y te imprima en cada caso si el color está en el arcoíris o no.
+// La cabecera de la función tendrá el siguiente aspecto: function isRainbow(colors)
+function isRainbow(colors: string[]){
+    let rainbowColors = ["Rojo", "Naranja", "Amarillo", "Verde", "Cian", "Azul", "Morado"];
+    for(let i = 0; i < colors.length; i++){
+        if(rainbowColors.indexOf(colors[i]) != -1){
+            console.log(`${colors[i]} está en el arcoiris`);
+        }else{
+            console.log(`${colors[i]} no está en el arcoiris`);
+        }
+    }
+}
+isRainbow(["Rosa", "Azul"]);
+//Realizar una función que te devuelva la suma del numero de caracteres de las palabras almacenadas en un array.
+//La cabecera de la función tendrá el siguiente aspecto: function add(myWords).
+function add(myWords: string[]) {
+    let sumaCaracteres = 0;
+  
+    for (let i = 0; i < myWords.length; i++) {
+      sumaCaracteres += myWords[i].length;
+    }
+  
+    return sumaCaracteres;
+  }
+  let myWords = ["Papá", "Mamá", "Hermano", "Hermana", "Abuela"];
+  console.log(add(myWords)); //¿Os podéis creer que me he tirado literalmente una hora para llegar a esta conclusión? Me siento un poco estupido xD
---
+++ retos-repaso1/condicionales.ts
@@ -0,0 +1,79 @@
+// 1. Trabajar en la rama repaso1 .En una carpeta nueva. Ej: retos1. Guarda estos retos en un
+// fichero .js Ej: condicionales.ts
+
+// 2. Realizar una función que dada una fecha de nacimiento te devuelva el signo zodiacal.
+
+// La fecha de nacimiento vendrá indicada por dos números: dia y mes.
+// La cabecera de la función tendrá el siguiente aspecto: function zodiac(day, month)
+function zodiac(dia: number, mes: string){
+    let result = "";
+    if((dia >= 22 && dia <= 30 && mes == "Diciembre") || dia >= 1 && dia <= 19 && mes == "Enero"){
+        result = "Tu signo zodiacal es: capricornio";
+    }else if((dia >= 20 && dia <= 30 && mes == "Enero") || dia >= 1 && dia <= 18 && mes == "Febrero"){
+        result = "Tu signo zodiacal es: acuario";
+    }else if((dia >= 19 && dia <= 30 && mes == "Febrero") || dia >= 1 && dia <= 20 && mes == "Marzo"){
+        result = "Tu signo zodiacal es: piscis";
+    }else if((dia >= 21 && dia <= 30 && mes == "Marzo") || dia >= 1 && dia <= 19 && mes == "Abril"){
+        result = "Tu signo zodiacal es: aries";
+    }else if((dia >= 20 && dia <= 30 && mes == "Abril") || dia >= 1 && dia <= 20 && mes == "Mayo"){
+        result = "Tu signo zodiacal es: tauro";
+    }else if((dia >= 21 && dia <= 30 && mes == "Mayo") || dia >= 1 && dia <= 20 && mes == "Junio"){
+        result = "Tu signo zodiacal es: geminis";
+    }else if((dia >= 21 && dia <= 30 && mes == "Junio") || dia >= 1 && dia <= 22 && mes == "Julio"){
+        result = "Tu signo zodiacal es: cancer";
+    }else if((dia >= 23 && dia <= 30 && mes == "Julio") || dia >= 1 && dia <= 22 && mes == "Agosto"){
+        result = "Tu signo zodiacal es: leo";
+    }else if((dia >= 23 && dia <= 30 && mes == "Agosto") || dia >= 1 && dia <= 22 && mes == "Septiembre"){
+        result = "Tu signo zodiacal es: virgo";
+    }else if((dia >= 23 && dia <= 30 && mes == "Sepriembre") || dia >= 1 && dia <= 22 && mes == "Octubre"){
+        result = "Tu signo zodiacal es: libra";
+    }else if((dia >= 23 && dia <= 30 && mes == "Octubre") || dia >= 1 && dia <= 21 && mes == "Noviembre"){
+        result = "Tu signo zodiacal es: escorpio";
+    }else if((dia >= 22 && dia <= 30 && mes == "Noviembre") || dia >= 1 && dia <= 21 && mes == "Diciembre"){
+        result = "Tu signo zodiacal es: sagitario";
+    }
+    return result;
+}
+console.log(zodiac(21, "Diciembre"));
+console.log(zodiac(1, "Enero"));
+// Realizar un procedimiento que dado el nombre de un país te imprima en que continente
+// estás. (Max 5 países por continente).
+// La cabecera del procedimiento tendrá el siguiente aspecto: function continent(country).
+let europa = ["España", "Francia", "Portugal", "Alemania", "Liberland"];
+let america = ["Estados Unidos", "Mexico", "Colombia", "Argentina", "Cuba"];
+let asia = ["China", "India", "Hong Kong", "Singapur", "Taiwan"];
+let oceania = ["Australia", "Nueva Zelanda", "Fiyi", "Samoa", "Tonga"];
+let africa = ["Marruecos", "Guinea Ecuatorial", "Argelia", "Tunez", "Congo"];
+function continent(country: string){
+    if(europa.indexOf(country) !== -1){
+        console.log("Estás en el continente europeo. Estos son algunos de sus países: " + europa);
+    }else if(africa.indexOf(country) !== -1){
+        console.log("Estás en el continente africano. Estos son algunos de sus países: " + africa);
+    }else if(asia.indexOf(country) !== -1){
+        console.log("Estás en el continen asiatico. Estos son algunos de sus países: " + asia);
+    }else if(america.indexOf(country) !== -1){
+        console.log("Estás en el continente americano. Estos son algunos de sus países: " + america);
+    }else if(oceania.indexOf(country) !== -1){
+        console.log("Estás en el continente oceánico. Estos son algunos de sus países: " + oceania);
+    }
+}
+continent("Congo");
+continent("España");
+continent("China");
+continent("Australia");
+continent("Mexico");
+// 4. Realizar una función que te imprima por consola el siguiente mensaje:
+// - “El numero es par”, si el numero introducido como parámetro de entrada es par
+// - “El numero es impar”, si el numero introducido como parámetro de entrada es impar
+// 5. La cabecera de la función tendrá el siguiente aspecto: function isEven(number)
+function isEven(number: number){
+    let result: string = "";
+    if(number % 2 == 0){
+        result = number + " es un número par.";
+    }else if(number % 2 != 0){
+        result = number + " es un número impar.";
+    }
+    return result;
+}
+console.log(isEven(4));
+console.log(isEven(5));
